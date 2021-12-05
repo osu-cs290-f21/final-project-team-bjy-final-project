f510778a3da13fbfdcdaf90971a71f60d0ae41a6
Jason Ho <ieatchagio123@10-249-11-15.wireless.oregonstate.edu>
2021-12-05 15:41:10 -0800

    Added instructions for javascript

---
+++ public/barber.js
@@ -0,0 +1,5 @@
+// add event listener when client submits an appointment
+// take in the inputed data
+// take each value and appoint it with a handlebar to make the post
+// have the handlebar create the post. 
+// change the bookPage 
